1ada4c9f54054e68a41979c60ae099ce108b31ad
cntencra <cn10cra@gmail.com>
2025-01-22 22:10:59 +0000

    Changing regex to use groups

--- Javascript/incrementString.js
+++ Javascript/incrementString.js
@@ -1,26 +1,25 @@
 
 export function incrementString (string) {
      
-    const regex = /([a-zA-Z]+)(\d+)/;
-    //upload to github and change above regex to /d+$/
+    const regex = /([a-zA-Z]+)(\d+$)/;
+    
     const match = string.match(regex);
+    console.log(match);
+    console.log(!match);
 
     if (!match) {
-        console.log(string + '1')
-        return string + '1'
+        return string + '1';
     }
 
-    console.log(match);
-    
     if (!match[2]) {
-        console.log(match);
+        return string + '1';
     }
-
+    
     const length = match[2].length;
     const number = Number(match[2]) + 1;
     const numberStr = String(number);
     
     const padded = numberStr.padStart(length, "0");
-    console.log(match[1] + padded); 
+    
     return match[1] + padded;
 }
