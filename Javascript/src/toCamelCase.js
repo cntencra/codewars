b061478f4a6f591f00c511471402615a7bee9505
cntencra <cn10cra@gmail.com>
2025-01-23 14:38:56 +0000

    Completing the toCamelCase challenge

--- Javascript/src/toCamelCase.js
+++ Javascript/src/toCamelCase.js
@@ -1,3 +1,15 @@
 export function toCamelCase(str) {
-    return 'ABC'
+
+    if (str.length === 0) {
+        return "";
+    }
+    const regex = /[_-]/
+
+    const words = str.split(regex);
+
+    for (let i = 1; i < words.length; i++) {
+        words[i] = words[i].slice(0,1).toUpperCase() + words[i].slice(1);
+    }
+
+    return words.join("");
 }
